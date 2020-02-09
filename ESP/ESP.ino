a828f226c0eab54f8b8b0da8db5b7f47bc0b820e
David Draxler <david_draxler@gmx.at>
2020-02-09 16:26:34 +0100

    W-Lan Funktioniert

--- ESP/ESP.ino
+++ ESP/ESP.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
-//Hello Welt
+
+
+
+
+#include <ESP8266WiFi.h>
+const char* ssid = "WFF";
+const char* password = "petersbachstrasse";
+ void setup() 
+{
+  Serial.begin(115200);
+  
+  delay(10);
+  Serial.println("Start");
+  WiFi.begin(ssid,password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi verbunden");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("IN LOOP");
+  delay(1000);
 
 }
