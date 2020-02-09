278f1badd818a21a8e7798255193ec89e85a0303
David Draxler <david_draxler@gmx.at>
2020-02-09 20:07:29 +0100

    Webserver eingerichtet

--- ESP/ESP.ino
+++ ESP/ESP.ino
@@ -5,6 +5,8 @@
 #include <ESP8266WiFi.h>
 const char* ssid = "WFF";
 const char* password = "petersbachstrasse";
+WiFiServer server(80);
+
  void setup() 
 {
   Serial.begin(115200);
@@ -19,10 +21,31 @@ const char* password = "petersbachstrasse";
   }
   Serial.println("");
   Serial.println("WiFi verbunden");
+  server.begin();
+  Serial.println("Server gestartet");
+  Serial.println(WiFi.localIP());
+  
+  
 }
 
-void loop() {
-  Serial.println("IN LOOP");
-  delay(1000);
-
+void loop() 
+{
+  WiFiClient client = server.available();
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("Connection: close");  // the connection will be closed after completion of the response
+  client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+  client.println();
+  client.println("<!DOCTYPE html>");
+  client.println("<html xmlns='http://www.w3.org/1999/xhtml'>");
+  client.println("<head>\n<meta charset='UTF-8'>");
+  client.println("<title>ESP8266 Temperature & Humidity DHT11 Sensor</title>");
+  client.println("</head>\n<body>");
+  client.println("<H2> Hallo Welt</H2>");
+  client.print("</body>\n</html>");
+  delay(100);
+ if(client)
+ {
+  Serial.println("Webseite wird abgerufen"); 
+ }
 }
