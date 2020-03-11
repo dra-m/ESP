29ee00d48c085a245c51612a96a5752104e94806
Michael Draxler <michael.draxler1@gmx.at>
2020-03-11 15:19:51 +0100

    Emailadressen in den Batchfiles hinzugefügt

--- ESP/David.bat
+++ ESP/David.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 TITLE Wechsel zu David
 call git config --global user.name "David Draxler"
-call git config --global user.email "."
+call git config --global user.email "david_draxler@gmx.at"
 Echo Aktueller Benutzer ist jetzt:
 call git config --global user.name
 PAUSE
--- ESP/Michael.bat
+++ ESP/Michael.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 TITLE Wechsel zu Michael
 call git config --global user.name "Michael Draxler"
-call git config --global user.email "."
+call git config --global user.email "michael.draxler1@gmx.at"
 Echo Aktueller Benutzer ist jetzt:
 call git config --global user.name
 PAUSE
